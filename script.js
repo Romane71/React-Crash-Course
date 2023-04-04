cc1ace793a7cfddc8eab1a739869e8498363b58e
Romane71 <romane.vanbroukhoven@aaoglobal.com>
2023-04-04 17:56:56 +0000

    Event listener

---
+++ script.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const incrementBtn = document.getElementById("increment-btn");
+const countDisplay = document.getElementById("count-display");
+
+incrementBtn.addEventListener("click", () => {
+  alert("You click me");
+});
