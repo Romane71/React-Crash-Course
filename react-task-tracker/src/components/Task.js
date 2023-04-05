3e080c1283296c1bca757fb0f2d760a670cf2201
Romane71 <120040498+Romane71@users.noreply.github.com>
2023-04-05 19:31:09 +0000

    styling icons

--- react-task-tracker/src/components/Task.js
+++ react-task-tracker/src/components/Task.js
@@ -4,7 +4,9 @@ const Task = ({ task}) => {
     return (
         <div className ='task'>
 
-            <h3>{task.text} <FaTimes /></h3>
+            <h3>{task.text} <FaTimes style={{ color: 'red', cursor: 'pointer'}} />
+            
+            </h3>
             <p>{task.day}</p>
 
 
