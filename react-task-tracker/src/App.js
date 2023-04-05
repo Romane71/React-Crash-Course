98208cba9db8c81f34aa2430373de27b1a04634d
Romane71 <120040498+Romane71@users.noreply.github.com>
2023-04-05 18:11:20 +0000

    adding tasks and looping them with .map

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -1,11 +1,13 @@
 'use strict'
 
 import Header from './components/Header'
+import Tasks from './components/Tasks'
 
  const App = () => {
  return (
  <div className='container'>
-  <Header title='Task Tracker'/>
+  <Header />
+  <Tasks />
 
   </div>
  )
