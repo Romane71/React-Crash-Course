848c3227f55c76383703135ff862571306796f3a
Romane71 <120040498+Romane71@users.noreply.github.com>
2023-04-05 18:01:26 +0000

    onClick PropTypes.func

--- react-task-tracker/src/components/Button.js
+++ react-task-tracker/src/components/Button.js
@@ -1,11 +1,8 @@
 
 import PropTypes from 'prop-types'
 
-const Button = ({color, text}) => {
-    const onClick = () => {
-        console.log('click')
-    }
-
+const Button = ({color, text, onClick}) => {
+ 
 return (
      <button onClick={onClick} style={{backgroundColor: color}} className="btn">{text}</button> )}
 
@@ -17,5 +14,6 @@ Button.defaultProps = {
 Button.propTypes = {
     text: PropTypes.string,
     color: PropTypes.string,
+    onClick: PropTypes.func,
 }
 export default Button
